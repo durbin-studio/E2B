10312442a110df0a9cab6203c26729b817dc003f
Md Waheduzzaman <Md Waheduzzaman>
2017-04-06 17:33:59 +0600

    working library with working code sample

--- app/src/main/java/com/aflion/e2bnumberchanger/ActivityToTest.java
+++ app/src/main/java/com/aflion/e2bnumberchanger/ActivityToTest.java
@@ -2,6 +2,7 @@ package com.aflion.e2bnumberchanger;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class ActivityToTest extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class ActivityToTest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_to_test);
+        //success check
+        Log.e("ActivityToTest","convert 123123:: bd= "+E2B.convertNumber("123123"));
+        //chek exception
+//        Log.e("ActivityToTest","Exception check convert 123123:: bd= "+E2B.convertNumber("12ld3123"));
     }
 }
--- e2bnumberchanger2/src/main/java/com/aflion/e2bnumberchanger/E2B.java
+++ e2bnumberchanger2/src/main/java/com/aflion/e2bnumberchanger/E2B.java
@@ -6,4 +6,65 @@ package com.aflion.e2bnumberchanger;
 
 public class E2B {
 
+    /**
+     * @param numberToConvert type String
+     * @return convertedNumber type String
+     * @throws NumberFormatException if any unwanted input is provided
+     * call this method with a number and it will return the converted bangla number **/
+    public static String convertNumber(String numberToConvert) throws NumberFormatException{
+        try{
+            String convertedNumber="";
+
+            char[] strTochar = new char[numberToConvert.length()];
+            strTochar = numberToConvert.toCharArray();
+
+            for(int i=0;i<strTochar.length;i++){
+                convertedNumber += checkNumber(strTochar[i]);
+            }
+
+            return convertedNumber;
+        }catch (Exception e){
+            throw new NumberFormatException();
+        }
+    }
+
+    //call to check the character
+    private static char checkNumber(char xChar){
+        char charToReturn = '০';
+        //convert the character to int
+        int xNumb = Integer.parseInt(""+xChar);
+        switch (xNumb){
+            case 0:
+                charToReturn='০';
+                break;
+            case 1:
+                charToReturn='১';
+                break;
+            case 2:
+                charToReturn='২';
+                break;
+            case 3:
+                charToReturn='৩';
+                break;
+            case 4:
+                charToReturn='৪';
+                break;
+            case 5:
+                charToReturn='৫';
+                break;
+            case 6:
+                charToReturn='৬';
+                break;
+            case 7:
+                charToReturn='৭';
+                break;
+            case 8:
+                charToReturn='৮';
+                break;
+            case 9:
+                charToReturn='৯';
+                break;
+        }
+        return  charToReturn;
+    }
 }
