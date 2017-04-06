5a230f16d5676260a0d258ff44ed782cd655048a
Md Waheduzzaman <Md Waheduzzaman>
2017-04-06 16:58:13 +0600

    initial commit

---
+++ app/src/main/java/com/aflion/e2bnumberchanger/ActivityToTest.java
@@ -0,0 +1,13 @@
+package com.aflion.e2bnumberchanger;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityToTest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_to_test);
+    }
+}
---
+++ e2bnumberchanger2/src/main/java/com/aflion/e2bnumberchanger/E2B.java
@@ -0,0 +1,9 @@
+package com.aflion.e2bnumberchanger;
+
+/**
+ * Created by user on 4/6/2017.
+ */
+
+public class E2B {
+
+}
